9cba55c17d383f2e3229f4477a8414e3b77ced8d
julio.gomes <julio.gomes@clicksoft.com.br>
2017-07-26 15:03:39 -0300

    correção token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  #protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session, if: ->{request.format.json?}
 
   # protect_from_forgery with: :null_session
   before_action :verified_request?
